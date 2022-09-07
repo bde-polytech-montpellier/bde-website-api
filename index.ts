cf696914dca8acd368512c650fb0c0af8d994b61
Lucas Nouguier <lucas.nouguier@protonmail.com>
2022-09-07 15:32:47 +0200

    Added rate limit to api calls

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import router from "./src/routes/router";
 import config from "./src/config/default";
 import bodyParser from "body-parser";
 import cloudinary from "cloudinary";
+import rateLimit from "express-rate-limit";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv"
@@ -11,6 +12,12 @@ const app = express();
 const port =
   process.env.PORT == null || process.env.PORT == "" ? 8000 : process.env.PORT;
 
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100, // Limit each IP to 100 requests per `window`
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 bodyParser.urlencoded({
   extended: true,
 });
@@ -24,6 +31,7 @@ cloudinary.v2.config({
 app.use(cors({ origin: config.client, optionsSuccessStatus: 204 }));
 app.use(bodyParser.json());
 app.use("/api", router);
+app.use("/api", limiter);
 
 app.listen(port, () => {
   console.log(`Server listening on the port::${port}`);
